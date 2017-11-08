454970c1465164dfb0742079aeaa04f394225c02
floris09 <florismeininger@gmail.com>
2017-11-08 17:46:05 +0100

    add update actioncreator

---
+++ src/actions/games/update.js
@@ -0,0 +1,29 @@
+import API from '../../api/client'
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  LOAD_ERROR,
+  LOAD_SUCCESS
+} from '../loading'
+
+
+const api = new API()
+
+export const updateGame = (game) => {
+  return dispatch => {
+    dispatch({ type: APP_LOADING })
+
+    api.patch(`/games/${game._id}`, {fields: [0,0,0,1,1,1,0,0,0]})
+      .then((result) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
+        })
+      })
+  }
+}
