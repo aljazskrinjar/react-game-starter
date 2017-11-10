2b058abd2687cc610bb4e74423c5ce75ca0e889a
floris09 <florismeininger@gmail.com>
2017-11-10 16:44:13 +0100

    delete game function

---
+++ src/actions/games/delete.js
@@ -0,0 +1,36 @@
+import API from '../../api/client'
+import {
+  APP_LOADING,
+  APP_DONE_LOADING,
+  LOAD_ERROR,
+  LOAD_SUCCESS
+} from '../loading'
+
+import { GAME_REMOVED } from './subscribe'
+
+
+const api = new API()
+
+export const removeGame = (gameId) => {
+  return dispatch => {
+    dispatch({ type: APP_LOADING })
+
+    api.get(`/games/${gameId}`)
+      .then((result) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({ type: LOAD_SUCCESS })
+
+        dispatch({
+          type: GAME_REMOVED,
+          payload: result.body
+        })
+      })
+      .catch((error) => {
+        dispatch({ type: APP_DONE_LOADING })
+        dispatch({
+          type: LOAD_ERROR,
+          payload: error.message
+        })
+      })
+  }
+}
