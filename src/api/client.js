63db7d795346eaedc6f1fd7fb76052498a7ef479
floris09 <florismeininger@gmail.com>
2017-11-10 12:27:23 +0100

    nothing

--- src/api/client.js
+++ src/api/client.js
@@ -8,7 +8,7 @@ export default class ApiClient {
   }
 
   constructor(host, options = {}) {
-    this.host = host || 'https://twenty-odometer.codaisseur.cloud/'
+    this.host = host || process.env.API_URL || 'http://localhost:3030'
     this.options = { ...this.defaultOptions, ...options }
   }
 
